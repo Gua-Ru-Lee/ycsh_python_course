d3b415ede5228c207cd6471b21f0bf2b72a4ddd9
Gua-Ru-Lee <j43037@gmail.com>
2020-10-20 16:09:48 +0800

    Initial commit

---
+++ L06/bmi.py
@@ -0,0 +1,6 @@
+h=float(input("請輸入身高（公分）:"))
+w=float(input("請輸入體重（公斤）:"))
+h=h/100
+bmi=w/h**2
+bmi=round(bmi,2)
+print("BMI",bmi)
---
+++ L06/triangle_area.py
@@ -0,0 +1,5 @@
+
+h=float(input("請輸入三角形的高:"))
+d=float(input("請輸入三角形的底邊長:"))
+area = (h*d/2) 
+print("三角形的面積是:",area)
